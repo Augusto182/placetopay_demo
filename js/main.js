6c5c81e0d44775bc2c75ae48cfc5cde8fa9dcb47
Augusto R. M <augustorm2018@gmail.com>
2018-02-23 12:22:36 -0500

    adding files

---
+++ js/main.js
@@ -0,0 +1,8 @@
+jq2 = jQuery.noConflict();
+jq2(function( $ ) {
+
+  $(function() {
+    console.log('PlaceToPlay log');
+  });
+
+});
---
+++ page.php
@@ -0,0 +1,43 @@
+<?php
+
+?>
+<!DOCTYPE HTML>
+
+<html>
+
+<head>
+	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
+	<title>PlaceToPay</title>
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/3.3.1/jquery.min.js"></script>
+	<script src="js/main.js"></script> 
+</head>
+
+<body>
+
+	<!--header>
+		<nav>
+			<ul>
+				<li>Your menu</li>
+			</ul>
+		</nav>
+	</header-->
+	
+	<section>
+	
+		
+	</section>
+
+	<!--aside>
+		<h2>About section</h2>
+		<p>Donec eu libero sit amet quam egestas semper. Aenean ultricies mi vitae est. Mauris placerat eleifend leo.</p>
+	</aside-->
+
+	<footer>
+		<p>Augusto R. M.</p>
+	</footer>
+
+</body>
+
+</html>
+
